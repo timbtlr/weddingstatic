2de2b03cb6149cc2ff48ac60c20ecda37edace28
Tym <tim.butler@trackmaven.com>
2015-09-13 16:04:33 -0400

    Styled the photo section to be a grid with popup

--- js/controllers/photo.control.js
+++ js/controllers/photo.control.js
@@ -13,41 +13,65 @@ angular
     .module('PhotoCtrl', [])
     .controller('PhotoController', function($scope, photoServiceFactory) {
         $scope.photo_list = [];
-        $scope.popupWindow = PopupHandler();
+
+        $scope.popup_window = new PopupHandler();
+        $scope.current_photo = null;
+        $scope.max_index = 0;
 
         $scope.queryForPhotos = function() {
             photoServiceFactory.get().then(function(result) {
                 $scope.photo_list = result.data.data;
+
+                //  Add an index value to each photo
+                for (var i = 0; i < $scope.photo_list.length; i++) {
+                    $scope.photo_list[i].index = i;
+                }
+
+                $scope.max_index = $scope.photo_list.length - 1;
             })
         }
 
-        $scope.setCurrentImage = function(image) {
-            $scope.current_image = image;
+        $scope.setCurrentPhoto = function(photo) {
+            $scope.current_photo = photo;
+            $scope.popup_window.openPopup();
+        };
+
+        $scope.closePhoto = function() {
+            $scope.popup_window.closePopup();
+        };
+
+        $scope.getNextPhoto = function() {
+            if ($scope.current_photo.index + 1 > $scope.max_index) {
+                $scope.current_photo = $scope.photo_list[0]
+            } else {
+                $scope.current_photo = $scope.photo_list[$scope.current_photo.index + 1]
+            }
+        };
+
+        $scope.getPreviousPhoto = function() {
+            if ($scope.current_photo.index - 1 < 0) {
+                $scope.current_photo = $scope.photo_list[$scope.max_index]
+            } else {
+                $scope.current_photo = $scope.photo_list[$scope.current_photo.index - 1]
+            }
         };
 
         //  Query for all photos
         $scope.queryForPhotos();
-
-        // initial image index
-        $scope.current_image = $scope.photo_list[0];
-
 });
 
 
 function PopupHandler () {
     this.show = false;
+    this.background_style = {};
 
     this.openPopup = function () {
-        this.windowShow = true;
+        this.backgroundStyle = {'opacity': '0.4', 'filter': 'alpha(opacity=40)'};
+        this.show = true;
     };
 
     this.closePopup = function () {
-        this.windowShow = false;
         this.backgroundStyle = {};
+        this.show = false;
     };
-
-    this.setResponse = function (message, color ) {
-        this.responseStyle = {"color" : color};
-        this.response = message;
-    }
 }

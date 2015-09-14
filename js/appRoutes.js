3163f66dfa9b884fef128f3cfb555380d2bee874
Tym <tim.butler@trackmaven.com>
2015-09-14 08:56:02 -0400

    Made some sryle changes

--- js/appRoutes.js
+++ js/appRoutes.js
@@ -1,7 +1,7 @@
 /*
 Name:
     appRoutes
-    
+
 Description:
 
 
@@ -16,40 +16,40 @@ angular
         .when('/', {
             templateUrl: '/templates/home.html'
         })
-        
+
         //  Query view
         .when('/about/', {
             templateUrl: '/templates/about.html'
         })
-        
+
         //  Query view
         .when('/photos/', {
             templateUrl: '/templates/photos.html',
             controller: 'PhotoController'
         })
-        
+
         //  Query view
         .when('/rsvp/', {
             templateUrl: 'templates/rsvp.html',
             controller: 'RsvpController'
         })
-        
+
         //  Query view
         .when('/registry', {
             templateUrl: 'templates/registry.html',
             controller: 'RegistryController'
         })
-        
+
         //  Query view
         .when('/contact', {
             templateUrl: 'templates/contact.html'
         })
-        
+
         //  Query view
         .when('/admin', {
             templateUrl: 'templates/admin.html'
         })
-        
+
         //  Redirect incorrect navigation to the default (Home) view
         .otherwise({ redirectTo: '/' });
 
